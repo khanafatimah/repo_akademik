703607abc3089b6d611088c8a41f179e7affc0e4
khanafatimah <khanafd338@gmail.com>
2025-03-10 11:47:08 +0700

    Create mahasiswa.php
    Buat modul mahasiswa

---
+++ mahasiswa.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>data Mahasiswa</title>
+</head>
+<body>
+    <h6>Data Mahasiswa AMP</h6>
+    <hr>
+    <h6>NIM : 23110016</h6><br>
+    <h6>Nama : Khana Fatimah Darsono</h6><br>
+    <a href="index.php">Kembali</a>
+</body>
+</html>
